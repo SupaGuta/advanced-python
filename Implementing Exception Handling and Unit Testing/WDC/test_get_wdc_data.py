60f7aea1572ba492ff4c1f28aa95de2eb5916131
Gustave Bernier <gustave.bernier@gmail.com>
2026-02-08 11:10:21 -0500

    Added course 3 module 2.2 assignements

---
+++ Implementing Exception Handling and Unit Testing/WDC/test_get_wdc_data.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from get_wdc_data import GetWDCData
+from sqlite3 import connect
+
+conn = connect("delivery.db")
+cursor = conn.cursor()
+cursor.execute("SELECT COUNT(*) FROM products")
+row_count = int(cursor.fetchone()[0])
+conn.close()
+
+class TestGetWDCData(TestCase):
+    def test_get_products(self):
+        self.assertEqual(len(GetWDCData.get_products()), row_count)
+
+    def test_get_prices(self):
+        self.assertEqual(len(GetWDCData.get_prices()), row_count)
